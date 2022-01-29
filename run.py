3cc4b3387424272576ddd4d10fd22274e5052f99
Ben Smith <bens6477@gmail.com>
2022-01-29 01:29:00 +0000

    Add about and contact pages with routing

--- run.py
+++ run.py
@@ -7,10 +7,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
 # Equivalent to
 # index = app.route("/")(index)
 # index()
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
